e87567b8e15a9a72b189a4a61fff0ef415860af4
Ron Teichner <ron.teichner@campus.technion.ac.il>
2019-11-16 22:46:52 +0200

    wip

--- test_speaker.py
+++ test_speaker.py
@@ -1,20 +1,24 @@
 #test_gender.py
 import os
-import cPickle
+#import cPickle
+import _pickle as cPickle
 import numpy as np
 from scipy.io.wavfile import read
+import matplotlib.pyplot as plt
+import scipy
 from speakerfeatures import extract_features
 import warnings
 warnings.filterwarnings("ignore")
 import time
 
+enableFigure = True
 
 #path to training data
-source   = "development_set\\"   
+source   = "./development_set/"
 
-modelpath = "speaker_models\\"
+modelpath = "./speaker_models/"
 
-test_file = "development_set_test.txt"        
+test_file = "./development_set_test.txt"
 
 file_paths = open(test_file,'r')
 
@@ -23,15 +27,27 @@ gmm_files = [os.path.join(modelpath,fname) for fname in
               os.listdir(modelpath) if fname.endswith('.gmm')]
 
 #Load the Gaussian gender Models
-models    = [cPickle.load(open(fname,'r')) for fname in gmm_files]
-speakers   = [fname.split("\\")[-1].split(".gmm")[0] for fname 
+models    = [cPickle.load(open(fname,'rb')) for fname in gmm_files]
+speakers   = [fname.split("/")[-1].split(".gmm")[0] for fname
               in gmm_files]
 
-# Read the test directory and get the list of test audio files 
-for path in file_paths:   
-    
+maxAudioLength = 0
+nFiles = 0
+for path in file_paths:
+    path = path.strip()
+    sr, audio = read(source + path)
+    maxAudioLength = np.maximum(maxAudioLength, audio.shape[0]) # samples
+    nFiles += 1
+
+maxAudioLength = maxAudioLength / sr # sec
+logLikelihoodMatrix = np.zeros((int(np.ceil(maxAudioLength / 10e-3)), nFiles, len(models)))
+speakersProbability = np.zeros((int(np.ceil(maxAudioLength / 10e-3)), nFiles, len(models)))
+
+# Read the test directory and get the list of test audio files
+file_paths = open(test_file,'r')
+for fIdx, path in enumerate(file_paths):
     path = path.strip()   
-    print path
+    print(path)
     sr,audio = read(source + path)
     vector   = extract_features(audio,sr)
     
@@ -39,11 +55,23 @@ for path in file_paths:
     
     for i in range(len(models)):
         gmm    = models[i]         #checking with each model one by one
-        scores = np.array(gmm.score(vector))
+        scores = np.array(gmm.score_samples(vector))
+        log_likelihood_alongTime = scores.cumsum()
+        logLikelihoodMatrix[:scores.shape[0], fIdx, i] = log_likelihood_alongTime
+        logLikelihoodMatrix[scores.shape[0]:, fIdx, i] = logLikelihoodMatrix[scores.shape[0] - 1, fIdx, i]
         log_likelihood[i] = scores.sum()
     
     winner = np.argmax(log_likelihood)
-    print "\tdetected as - ", speakers[winner]
-    time.sleep(1.0)
+    print("\tdetected as - ", speakers[winner])
+    #time.sleep(1.0)
+
+    speakersProbability[:, fIdx, :] = scipy.special.softmax(logLikelihoodMatrix[:, fIdx, :], axis=1)
+
+    if enableFigure:
+        tVec = np.arange(0, speakersProbability.shape[0]) * 10e-3 + (25e-3) / 2
+        plt.plot(tVec, speakersProbability[:, fIdx, winner])
+        plt.xlabel('sec')
+        plt.ylim(-0.1, 1.1)
+        plt.title('winner speaker prob vs time')
 
 
--- train_models.py
+++ train_models.py
@@ -1,21 +1,23 @@
 #train_models.py
 
-import cPickle
+#import cPickle
+import _pickle as cPickle
 import numpy as np
 from scipy.io.wavfile import read
-from sklearn.mixture import GMM 
+#from sklearn.mixture import GMM
+from sklearn.mixture import GaussianMixture
 from speakerfeatures import extract_features
 import warnings
 warnings.filterwarnings("ignore")
 
 
 #path to training data
-source   = "development_set\\"   
+source   = "./development_set/"
 
 #path where training speakers will be saved
-dest = "speaker_models\\"
+dest = "./speaker_models/"
 
-train_file = "development_set_enroll.txt"        
+train_file = "./development_set_enroll.txt"
 
 
 file_paths = open(train_file,'r')
@@ -26,7 +28,7 @@ count = 1
 features = np.asarray(())
 for path in file_paths:    
     path = path.strip()   
-    print path
+    print(path)
     
     # read the audio
     sr,audio = read(source + path)
@@ -39,14 +41,14 @@ for path in file_paths:
     else:
         features = np.vstack((features, vector))
     # when features of 5 files of speaker are concatenated, then do model training
-    if count == 5:    
-        gmm = GMM(n_components = 16, n_iter = 200, covariance_type='diag',n_init = 3)
+    if count == 5:
+        gmm = GaussianMixture(n_components=16, max_iter=200, covariance_type='diag', n_init=3)
         gmm.fit(features)
         
         # dumping the trained gaussian model
         picklefile = path.split("-")[0]+".gmm"
-        cPickle.dump(gmm,open(dest + picklefile,'w'))
-        print '+ modeling completed for speaker:',picklefile," with data point = ",features.shape    
+        cPickle.dump(gmm,open(dest + picklefile,'wb'))
+        print('+ modeling completed for speaker:',picklefile," with data point = ",features.shape)
         features = np.asarray(())
         count = 0
     count = count + 1
